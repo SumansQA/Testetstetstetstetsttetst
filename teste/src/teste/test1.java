3eae410d9365baff291f9100ac6bbc3ae0f6dd0a
suman s <suman.s@housejoy.in>
2021-02-17 08:58:52 +0530

    changes

---
+++ teste/src/teste/test1.java
@@ -0,0 +1,14 @@
+package teste;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test1 {
+	
+	public static void main(String[] args) {
+		System.getProperty(key)
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.housejoy.in");
+	}
+
+}
